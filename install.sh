88d2ee6449a2ae3ec4fad831a738878b12ee86ac
rileycalhoun <rileycalhoun@users.noreply.github.com>
2026-09-19 12:40:31 -0700

    installer defaults to v0.6.6

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -e
 
 REPO="black-candle-technologies/courier"
-VERSION="${COURIER_VERSION:-v0.6.5}"
+VERSION="${COURIER_VERSION:-v0.6.6}"
 
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m)"
